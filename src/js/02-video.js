b241a8f34e03efaa49a8298c6b3f999fffe8397e
Pavlo Tereshko <pavlo.tereshko@gmail.com>
2023-11-09 21:05:40 +0200

    Task 02 was added

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const LOCAL_STORAGE_KEY = 'playbackTime';
+const player = new Player(document.querySelector('iframe'));
+
+player.setCurrentTime(Number(localStorage.getItem(LOCAL_STORAGE_KEY)));
+
+player.on(
+  'timeupdate',
+  throttle(
+    data =>
+      localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(data.seconds)),
+    1000
+  )
+);
