29b0bc98b0b6f3179caf140351e3ed62d91b84f0
Pavlo Tereshko <pavlo.tereshko@gmail.com>
2023-11-09 21:03:29 +0200

    Task 01 was added

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
-// Add imports above this line
 import { galleryItems } from './gallery-items';
+
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+// Add imports above this line
 // Change code below this line
 
-console.log(galleryItems);
+function createItems(galleryItems) {
+  const gallery = document.querySelector('.gallery');
+
+  const items = galleryItems
+    .map(({ preview, description, original }) => (
+      `<li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            alt="${description}"
+          />
+        </a>
+      </li>`
+    ))
+    .join('');
+
+  gallery.insertAdjacentHTML('afterbegin', items);
+}
+
+createItems(galleryItems);
+
+const lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250});
