847971ebe6eaa6c7b4771f1bbf4fd6b9c4316d37
fff500 <pavlo.tereshko@gmail.com>
2023-11-10 15:16:43 +0000

    Deploying to gh-pages from @ fff500/goit-advancedjs-hw-01@af96bf9f451804f6fdc5d2f3d69c5814173dc205 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,2 +1,2 @@
-import"./assets/modulepreload-polyfill-ec808ebb.js";/* empty css                      */import{P as r,l as a}from"./assets/vendor-78be7656.js";const e="playbackTime",t=new r(document.querySelector("iframe"));t.setCurrentTime(Number(localStorage.getItem(e)));t.on("timeupdate",a(o=>localStorage.setItem(e,JSON.stringify(o.seconds)),1e3));
+import"./assets/modulepreload-polyfill-ec808ebb.js";/* empty css                      */import{P as r,l as a}from"./assets/vendor-78be7656.js";const e="playback-time",t=new r(document.querySelector("iframe"));t.setCurrentTime(Number(localStorage.getItem(e)));t.on("timeupdate",a(o=>localStorage.setItem(e,JSON.stringify(o.seconds)),1e3));
 //# sourceMappingURL=commonHelpers2.js.map
